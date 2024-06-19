98b775d48d708b8a580f702541e29edf9d01147e
Albion Berisha <albion.b@gjirafa.com>
2024-06-19 13:50:39 +0200

    Add project files.

---
+++ Customer.Microservice/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace Customer.Microservice.Data
+{
+    public class ApplicationDbContext : DbContext,IApplicationDbContext
+    {
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Entities.Customer> Customers{ get; set; }
+
+        public async Task<int> SaveChanges()
+        {
+            return await base.SaveChangesAsync();
+        }
+    }
+}
